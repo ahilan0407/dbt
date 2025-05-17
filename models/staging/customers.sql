07770b83fe818385b2e41504c48f0ec49ae645e8
GNANASAMBANDHAM BALASUNDARAM <ahilan.gnanam@gmail.com>
2025-05-17 07:02:26 +0000

    commit

--- models/staging/customers.sql
+++ models/staging/customers.sql
@@ -1,9 +1,5 @@
 {{ config(materialized='table') }}
 
-WITH tb1 as(
     select
-    id ,
-    first_name,
-    last_name
-    from {{source('datafeed_shared_schema','stg_customerdata')}})
-select * from tb1
+    *
+    from {{source('datafeed_shared_schema','stg_customerdata')}}
