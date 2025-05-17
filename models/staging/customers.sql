1df0cdc9a1a9051a6fc370b0c981928d1ab38c83
GNANASAMBANDHAM BALASUNDARAM <ahilan.gnanam@gmail.com>
2025-05-17 06:57:52 +0000

    COMMIT

--- models/staging/customers.sql
+++ models/staging/customers.sql
@@ -1,1 +1,9 @@
-select * from DBT_DB.STAGING_LAYER.STG_CUSTOMERS
+{{ config(materialized='table') }}
+
+WITH tb1 as(
+    select
+    id ,
+    first_name,
+    last_name
+    from {{source('datafeed_shared_schema','stg_customerdata')}})
+select * from tb1
